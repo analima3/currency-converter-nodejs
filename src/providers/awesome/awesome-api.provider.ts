20917744eaaa3d3c498b7c8dfc8e751cc3e413d2
analima3 <af.rl@hotmail.com>
2026-08-01 13:13:02 -0400

    fix: remove console.log e aplica formatação no arquivo .provider

--- src/providers/awesome/awesome-api.provider.ts
+++ src/providers/awesome/awesome-api.provider.ts
@@ -1,24 +1,24 @@
-import { env } from "../../configs/env.ts";
-import { ExternalApiError } from "../../shared/errors/external-api-error.ts";
-import { AWESOME_API_URL } from "./awesome-api.constants.ts";
-import { AwesomeApiResponse, ExchangeRate } from "./awesome-api.types.ts";
+import { env } from "../../configs/env.ts"
+import { ExternalApiError } from "../../shared/errors/external-api-error.ts"
+import { AWESOME_API_URL } from "./awesome-api.constants.ts"
+import type { AwesomeApiResponse, ExchangeRate } from "./awesome-api.types.ts"
 
 export class AwesomeApiProvider {
-    async getExchangeRate(): Promise<ExchangeRate> {
-        const response = await fetch(AWESOME_API_URL, {
-            headers: {
-                'x-api-key': env.AWESOME_API_KEY
-            }
-        })
+  async getExchangeRate(): Promise<ExchangeRate> {
+    const response = await fetch(AWESOME_API_URL, {
+      headers: {
+        "x-api-key": env.AWESOME_API_KEY,
+      },
+    })
 
-        if (!response.ok) {
-            throw new ExternalApiError()
-        }
-
-        const data = await response.json() as AwesomeApiResponse
-    
-        return {
-            exchangeRate: Number(data.BRLUSD.bid)
-        }
+    if (!response.ok) {
+      throw new ExternalApiError()
     }
+
+    const data = (await response.json()) as AwesomeApiResponse
+
+    return {
+      exchangeRate: Number(data.BRLUSD.bid),
+    }
+  }
 }
--- src/shared/errors/error-handler.ts
+++ src/shared/errors/error-handler.ts
@@ -1,23 +1,20 @@
-import type { FastifyReply, FastifyRequest } from 'fastify'
+import type { FastifyReply } from "fastify"
+import { AppError } from "./app-error.ts"
+import { ErrorCode } from "./error-codes.ts"
+import { HttpStatus } from "./http-status.ts"
 
-import { HttpStatus } from './http-status.ts';
-import { ErrorCode } from './error-codes.ts';
-import { AppError } from './app-error.ts';
-
-export function errorHandler(error: unknown, request: FastifyRequest, reply: FastifyReply) {
-  console.log('passei pelo error handler');
-  
-    if (error instanceof AppError) {
-        return reply.status(error.statusCode).send({
-            statusCode: error.statusCode,
-            code: error.code,
-            message: error.message,
-        })
-    }
-
-    return reply.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
-        code: ErrorCode.INTERNAL_SERVER_ERROR,
-        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
-        message: 'Erro interno do servidor.'
+export function errorHandler(error: unknown, reply: FastifyReply) {
+  if (error instanceof AppError) {
+    return reply.status(error.statusCode).send({
+      code: error.code,
+      message: error.message,
+      statusCode: error.statusCode,
     })
+  }
+
+  return reply.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
+    code: ErrorCode.INTERNAL_SERVER_ERROR,
+    message: "Erro interno do servidor.",
+    statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+  })
 }
