2e7aa8573acb90a94b411c812c7ca157846b0cb6
analima3 <af.rl@hotmail.com>
2026-08-03 12:35:08 -0400

    refactor: cria constants/index para reexportar arquivos

---
+++ src/shared/utils/constants/index.ts
@@ -0,0 +1,6 @@
+export {
+  CurrencyCode,
+  currencyCodeEnumToArray,
+} from "./currency-code.constant.ts"
+export { ErrorCode, type ErrorCodeType } from "./error-codes.constant.ts"
+export { HttpStatus, type HttpStatusType } from "./http-status.constant.ts"
