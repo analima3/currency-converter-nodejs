a4e90e34382814a13deab6aba7e3ac2878e500c2
analima3 <af.rl@hotmail.com>
2026-08-03 12:40:44 -0400

    refactor: import arquivos de constant/index

--- src/shared/types/errors.types.ts
+++ src/shared/types/errors.types.ts
@@ -1,5 +1,4 @@
-import type { ErrorCodeType } from "../utils/constants/error-codes.constants.ts"
-import type { HttpStatusType } from "../utils/constants/http-status.constants.ts"
+import type { ErrorCodeType, HttpStatusType } from "../utils/constants/index.ts"
 
 export interface AppErrorOptions {
   code: ErrorCodeType
